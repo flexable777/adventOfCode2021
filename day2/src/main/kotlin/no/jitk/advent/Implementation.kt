09b45f2195438df77a5a0a37db71b528a6a06ead
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-01 08:11:40 +0100

    Prepare for day 2

---
+++ day2/src/main/kotlin/no/jitk/advent/Implementation.kt
@@ -0,0 +1,12 @@
+package no.jitk.advent
+
+class Implementation {
+
+    fun processPart1(lines: List<String>): Int {
+        return 0
+    }
+
+    fun processPart2(lines: List<String>): Int {
+        return 0
+    }
+}
