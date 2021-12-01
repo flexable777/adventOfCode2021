e9582e7e4600b888d01147a11137b3f4c5abdf0e
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-01 20:46:51 +0100

    Some more refactoring

--- day1/src/main/kotlin/no/jitk/advent/Implementation.kt
+++ day1/src/main/kotlin/no/jitk/advent/Implementation.kt
@@ -1,11 +1,8 @@
 package no.jitk.advent
 
-class Implementation {
+fun processPart1(lines: List<Long>) =
+    lines.windowed(size = 2).filter { it[1] > it[0] }.size
 
-    fun processPart1(lines: List<Long>) =
-        lines.windowed(size = 2).filter { it[1] > it[0] }.size
-
-    fun processPart2(lines: List<Long>) =
-        lines.map { it.toInt() }.windowed(size = 3).zipWithNext()
-            .filter { it.second.sum() > it.first.sum() }.size
-}
+fun processPart2(lines: List<Long>) =
+    lines.map { it.toInt() }.windowed(size = 3).zipWithNext()
+        .filter { it.second.sum() > it.first.sum() }.size
