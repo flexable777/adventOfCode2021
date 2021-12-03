fedd26099575e772500e7b38b9be5676806b90dd
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-03 16:53:54 +0100

    Day three refactoring

--- day3/src/main/kotlin/no/jitk/advent/Implementation.kt
+++ day3/src/main/kotlin/no/jitk/advent/Implementation.kt
@@ -3,20 +3,12 @@ package no.jitk.advent
 class Implementation3 {
 
     fun processPart1(lines: List<String>): Int {
-        var gamma = ""
-
-        for (i in lines[0].indices) {
-            gamma += if (lines.map { it[i] }.count { it == '1' } > lines.size / 2) {
-                "1"
-            } else {
-                "0"
-            }
+        val gamma = lines[0].indices.joinToString(separator = "") { i ->
+            if (lines.map { l -> l[i] }.count { it == '1' } > lines.size / 2) "1" else "0"
         }
+
         //TODO Should be able to inverse the bits for epsilon rate
-        return gamma.toInt(2) * gamma
-            .replace("1", "x")
-            .replace("0", "1")
-            .replace("x", "0")
+        return gamma.toInt(2) * gamma.map { if (it == '0') '1' else '0' }.joinToString(separator = "")
             .toInt(2)
     }
 
@@ -32,13 +24,13 @@ class Implementation3 {
     ): String {
         val remainingLines = lines.toMutableList()
         for (i in lines[0].indices) {
-            if (remainingLines.size == 1) {
-                return remainingLines.first()
-            }
             if (remainingLines.size / 2 >= remainingLines.count { it[i] == '0' }) {
                 remainingLines.removeIf { it[i] == if (wantsOxygenRating) '0' else '1' }
             } else {
                 remainingLines.removeIf { it[i] == if (wantsOxygenRating) '1' else '0' }
+            }
+            if (remainingLines.size == 1) {
+                return remainingLines.first()
             }
         }
         throw RuntimeException("How can this be?")
