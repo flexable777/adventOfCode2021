8d33dccb30b27b14cebd26a9590f6d07cc4e0598
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-04 10:18:14 +0100

    Solution day 4. Hopefully it can be refactored.

--- day4/src/main/kotlin/no/jitk/advent/day4/Implementation.kt
+++ day4/src/main/kotlin/no/jitk/advent/day4/Implementation.kt
@@ -1,10 +1,88 @@
 package no.jitk.advent.day4
 
 
-fun processDay4Part1(lines: List<String>): Int {
-    return 0
+fun processDay4Part1(lines: MutableList<String>): Int {
+    val (draws, boards) = pair(lines)
+
+    val drawn = mutableListOf<Int>()
+
+    for (draw in draws) {
+        drawn.add(draw)
+
+        //exists in any board?
+        if (drawn.size >= 5) {
+            for ((i, board) in boards.withIndex()) {
+                for (s in board.setOfPossibleRows) {
+                    if (drawn.containsAll(s)) {
+                        println("Bingo! Winner is board $i with numbers $s")
+                        return board.setOfPossibleRows.flatten().toSet().filter { it !in drawn }.sum() * draw
+                    }
+                }
+            }
+        }
+    }
+    throw RuntimeException("something's wrong")
 }
 
-fun processDay4Part2(lines: List<String>): Int {
-    return 0
+fun processDay4Part2(lines: MutableList<String>): Int {
+    val (draws, boards) = pair(lines)
+
+    val drawn = mutableListOf<Int>()
+
+    for (draw in draws) {
+        drawn.add(draw)
+
+        //exists in any board?
+        if (drawn.size >= 5) {
+            for ((i, board) in boards.withIndex()) {
+                for (row in board.setOfPossibleRows) {
+                    if (drawn.containsAll(row) && draw in row) {
+                        if (isOnlyOneBoardLeft(boards) && !board.hasWon) {
+                            println("The last winner")
+                            println("Winner is board $i with numbers $row and draw is $draw")
+                            return board.setOfPossibleRows.flatten().toSet().filter { it !in drawn }.sum() * draw
+                        }
+                        board.hasWon = true
+                    }
+                }
+            }
+        }
+    }
+    throw RuntimeException("What?")
 }
+
+private fun isOnlyOneBoardLeft(boards: List<Board>) = boards.count { !it.hasWon } == 1
+
+private fun pair(lines: MutableList<String>): Pair<List<Int>, List<Board>> {
+    val draws = lines.first().split(",").map { it.toInt() }
+
+    val sublist = lines.subList(2, lines.size)
+    sublist.removeIf { it.isBlank() }
+
+    //Boards with all the rows
+    val boards = sublist.windowed(size = 5, step = 5).map { rowsOfStrings ->
+        val board = Board()
+
+        rowsOfStrings.forEach {
+            board.setOfPossibleRows.add(it.replace("  ", " ").trim().split(" ").map { a -> a.toInt() })
+        }
+        board
+    }
+
+    //Add all the columns also
+    boards.map { board ->
+        for (i in 0 until 5) {
+            val cols = mutableListOf<Int>()
+            for (row in board.setOfPossibleRows.subList(0, 5)) {
+                cols.add(row[i])
+            }
+            board.setOfPossibleRows.add(cols)
+        }
+    }
+    return Pair(draws, boards)
+}
+
+data class Board(
+    val setOfPossibleRows: MutableList<List<Int>> = mutableListOf(),
+    var hasWon: Boolean = false
+)
--- day4/src/test/kotlin/no/jitk/advent/day4/ImplementationTest.kt
+++ day4/src/test/kotlin/no/jitk/advent/day4/ImplementationTest.kt
@@ -9,14 +9,14 @@ internal class ImplementationTest {
 
     @Test
     fun `original test part 1`() {
-        val lines = File("src/test/resources/test_data.txt").readLines()
+        val lines = File("src/test/resources/test_data.txt").readLines().toMutableList()
 
-        assertThat(processDay4Part1(lines)).isEqualTo(0)
+        assertThat(processDay4Part1(lines)).isEqualTo(4512)
     }
 
     @Test
     fun `actual run part 1`() {
-        val lines = File("src/test/resources/actual_data.txt").readLines()
+        val lines = File("src/test/resources/actual_data.txt").readLines().toMutableList()
 
         val millis = measureTimeMillis {
             println("Actual run part 1: " + processDay4Part1(lines))
@@ -26,20 +26,19 @@ internal class ImplementationTest {
 
     @Test
     fun `original test part 2`() {
-        val lines = File("src/test/resources/test_data.txt").readLines()
+        val lines = File("src/test/resources/test_data.txt").readLines().toMutableList()
 
-        assertThat(processDay4Part2(lines)).isEqualTo(0)
+        assertThat(processDay4Part2(lines)).isEqualTo(1924)
     }
 
     @Test
     fun `actual run part 2`() {
-        val lines = File("src/test/resources/actual_data.txt").readLines()
+        val lines = File("src/test/resources/actual_data.txt").readLines().toMutableList()
 
         val millis = measureTimeMillis {
             println("Actual run part 2: " + processDay4Part2(lines))
         }
         println("part 2 took $millis millis to run")
-
     }
 
 }
