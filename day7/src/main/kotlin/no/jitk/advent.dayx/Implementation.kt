7d1ede2ba2779eaefc4a68e50f0fb82c7c8c0643
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-07 11:56:04 +0100

    Day 7. Pretty straight forward, but I have seen better solutions using gauss.

---
+++ day7/src/main/kotlin/no/jitk/advent.dayx/Implementation.kt
@@ -0,0 +1,50 @@
+package no.jitk.advent.dayx
+
+import kotlin.math.absoluteValue
+
+fun processDay7Part1(lines: List<String>): Int {
+    val positions = lines.first().split(",").map { it.toInt() }
+
+    val maxValue = positions.maxOrNull()!!
+    var min = Int.MAX_VALUE
+
+    for (i in 0 until maxValue) {
+        var cost = 0
+        positions.forEach { pos ->
+            val absoluteValue = (pos - i).absoluteValue
+            cost += absoluteValue
+        }
+        if (cost < min) {
+            min = cost
+        }
+    }
+    return min
+}
+
+fun processDay7Part2(lines: List<String>): Int {
+    val positions = lines.first().split(",").map { it.toInt() }
+    val maxValue = positions.maxOrNull()!!
+    var min = Int.MAX_VALUE
+    for (i in 0 until maxValue) {
+        var cost = 0
+        positions.forEach { pos ->
+            cost += if (i < pos) {
+                moveCrab(i, pos, 0, 1)
+            } else {
+                moveCrab(pos, i, 0, 1)
+            }
+        }
+        if (cost < min) {
+            min = cost
+        }
+    }
+
+    return min
+}
+
+fun moveCrab(pos: Int, end: Int, cost: Int, step: Int): Int {
+    if (pos == end) {
+        return cost
+    }
+    return moveCrab(pos + 1, end, cost + step, step + 1)
+}
