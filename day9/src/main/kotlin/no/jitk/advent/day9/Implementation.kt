1f83a70dfdf30dadb2e6815c1fb49fd53e58eaeb
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-08 21:00:55 +0100

    Prepare for day 9

---
+++ day9/src/main/kotlin/no/jitk/advent/day9/Implementation.kt
@@ -0,0 +1,11 @@
+package no.jitk.advent.day9
+
+fun processDay9Part1(lines: List<String>): Int {
+
+
+    return 0
+}
+
+fun processDay9Part2(lines: List<String>): Int {
+    return 0
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,4 +9,4 @@
  */
 
 rootProject.name = "adventOfCode2021"
-include("day8")
+include("day9")
