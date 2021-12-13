960d3b2a853847221bbf569864f1fa5e9282a65e
Andreas Jonsson <andreas.jonsson@nav.no>
2021-12-13 21:44:58 +0100

    Prepare for day 14

---
+++ day14/src/main/kotlin/no/jitk/advent/day14/Implementation.kt
@@ -0,0 +1,9 @@
+package no.jitk.advent.day14
+
+fun processDay14Part1(lines: List<String>): Int {
+    return -1
+}
+
+fun processDay14Part2(lines: List<String>): Int {
+    return 0
+}
---
+++ day14/src/test/kotlin/no/jitk/advent/day14/ImplementationTest.kt
@@ -0,0 +1,45 @@
+package no.jitk.advent.day14
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+internal class ImplementationTest {
+
+    @Test
+    fun `original test part 1`() {
+        val lines = File("src/test/resources/test_data.txt").readLines()
+
+        assertThat(processDay14Part1(lines)).isEqualTo(0)
+    }
+
+    @Test
+    fun `actual run part 1`() {
+        val lines = File("src/test/resources/actual_data.txt").readLines()
+
+        val millis = measureTimeMillis {
+            println("Actual run part 1: " + processDay14Part1(lines))
+        }
+        println("part 1 took $millis millis to run")
+    }
+
+    @Test
+    fun `original test part 2`() {
+        val lines = File("src/test/resources/test_data.txt").readLines()
+
+        assertThat(processDay14Part2(lines)).isEqualTo(0)
+    }
+
+    @Test
+    fun `actual run part 2`() {
+        val lines = File("src/test/resources/actual_data.txt").readLines()
+
+        val millis = measureTimeMillis {
+            println("Actual run part 2: " + processDay14Part2(lines))
+        }
+        println("part 2 took $millis millis to run")
+
+    }
+
+}
